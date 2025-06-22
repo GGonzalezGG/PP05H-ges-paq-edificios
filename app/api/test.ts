062b6f110e16242eac626f86b06dfe84ab1394a4
GGonzalezGG <145786122+GGonzalezGG@users.noreply.github.com>
2025-06-21 21:25:14 -0400

    PP05H-60 Landing page redirecciona al login y más cositas
    Limpieza de archivos en desuso, cambio del favicon y de los titulos de las paginas

--- app/api/test.ts
+++
@@ -1,8 +0,0 @@
-import { getAllUsers } from "../db/statements.ts";
-
-try {
-  const users = getAllUsers();
-  console.log("Usuarios:", users);
-} catch (error) {
-  console.error("Error:", error.message);
-}
